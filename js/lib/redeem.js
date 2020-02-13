fa09305d213d9b7a2fe12efc72c246b460550980
田蕾 <tlchloe3399@gmail.com>
2020-02-14 10:03:32 +1100

    Initial commit

---
+++ js/lib/redeem.js
@@ -0,0 +1,37 @@
+$(".re100").click(function () {
+    alert("兑换成功");
+});
+$(".re20").click(function () {
+    alert("兑换成功");
+});
+$(".re15").click(function () {
+    alert("兑换成功");
+});
+$(".re25").click(function () {
+    alert("兑换成功");
+});
+
+$(".re30").click(function () {
+    alert("兑换成功");
+});
+$(".re35").click(function () {
+    alert("兑换成功");
+});
+$(".re55").click(function () {
+    alert("兑换成功");
+});
+
+$(".re75").click(function () {
+    alert("兑换成功");
+});
+$(".re150").click(function () {
+    alert("兑换成功");
+});
+$(".re180").click(function () {
+    alert("兑换成功");
+});
+$(".re50").click(function () {
+    alert("兑换成功");
+});
+
+var score = new Firebase('https://giftofgua.firebaseio.com');
